c109198e039446cb942c7ef7d31a9a91aa476e9d
Viraj Mehta <54108753+viraj-coder@users.noreply.github.com>
2021-01-11 15:03:51 +0530

    Add files via upload

---
+++ PRO-C153-Student-Activity-main/PRO-C153-Student-Activity-main/components/bird.js
@@ -0,0 +1,31 @@
+AFRAME.registerComponent("flying-birds", {
+init:function(){
+    for(var i=1;i<=20;i++){
+        var id=`hurdles${i}`
+        var posX=(Math.random()*3000+(-1000))
+        var posY=(Math.random()*2+(-1))
+        var posZ=(Math.random()*3000+(-1000))
+        var position={x:posX,y:posY,z:posZ}
+        this.flyingBirds(id,position)
+    }
+},
+flyingBirds:(id,position)=>{
+    var terrainEl=document.querySelector("#terrain")
+    var birdEl=document.createElement("a-entity")
+    birdEl.setAttribute("id",id)
+    birdEl.setAttribute("id",id)
+    birdEl.setAttribute("scale",{x:500,y:500,z:500})
+    birdEl.setAttribute("gltf-model","./bird/scene.gltf")
+    birdEl.setAttribute("animation-mixer",{})
+    terrainEl.appendChild(birdEl)
+    birdEl.setAttribute("static-body",{
+        shape:"sphere",
+        sphereRadius:5
+        
+    })
+
+birdEl.setAttribute("game-play",{
+    elementId:`#${id}`
+})
+}
+})
